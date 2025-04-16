cbf9ed262fccf6659bb04f897d670d056d2b381d
homeqube301 <homeqube301@gmail.com>
2025-04-16 16:06:35 +0900

    first.commit

---
+++ app/src/main/java/com/example/maps_2025_4_14/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.maps_2025_4_14
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.maps_2025_4_14.ui.theme.Maps_2025_4_14Theme
+import com.google.maps.android.compose.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.CameraPosition
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Maps_2025_4_14Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MapScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MapScreen() {
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Marker in Singapore"
+        )
+    }
+}
