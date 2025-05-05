a755a76147547e419dfd927688d5a0119a14cbea
homeqube301 <homeqube301@gmail.com>
2025-05-05 16:18:17 +0900

    update:簡易検索のUIを改善、結果をアウトラインで囲むように

--- app/src/main/java/com/mKanta/archivemaps/ui/screen/map/SearchMaker.kt
+++ app/src/main/java/com/mKanta/archivemaps/ui/screen/map/SearchMaker.kt
@@ -1,5 +1,6 @@
 package com.mKanta.archivemaps.ui.screen.map
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -23,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -68,27 +71,45 @@ fun SearchMaker(
 
             val results = if (isSearchingByTitle) titleResults else memoResults
             if (results.isEmpty()) {
-                Text("検索結果がありません", modifier = Modifier.padding(8.dp))
+                Text(
+                    text = "検索結果がありません",
+                    fontSize = 16.sp,
+                    fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                    color = Color.Gray,
+                    modifier =
+                        Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(40.dp),
+                )
             } else {
-                results.forEach { marker ->
-                    Text(
-                        text = if (isSearchingByTitle) marker.title else "${marker.title}（メモ一致）",
-                        modifier =
-                            Modifier
-                                .fillMaxWidth()
-                                .clickable {
-                                    if (isSearchingByTitle) {
-                                        onMarkerTapped(marker)
-                                    } else {
-                                        onMemoTapped(marker)
-                                    }
-                                }.padding(8.dp),
-                    )
+                Column(
+                    modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                ) {
+                    results.forEach { marker ->
+
+                        Box(
+                            modifier =
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 4.dp)
+                                    .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+                                    .clickable {
+                                        if (isSearchingByTitle) {
+                                            onMarkerTapped(marker)
+                                        } else {
+                                            onMemoTapped(marker)
+                                        }
+                                    }.padding(16.dp),
+                        ) {
+                            Text(
+                                text = if (isSearchingByTitle) marker.title else "${marker.title}（メモ一致）",
+                            )
+                        }
+                    }
                 }
             }
         }
 
-        // 入力欄を画面下に固定
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier =
